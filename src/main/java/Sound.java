39b6263aeb99c9f7ee17a50b38086303845706e6
Sleepykevin1 <Hunterkevin12398@gmail.com>
2025-04-25 22:55:18 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -1,10 +1,14 @@
 import java.util.*;
+
 public class Sound
 {
   /** the array of values in this sound; guaranteed not to be null */
-  int[] samples;
+  private int[] samples;
 
-
+  public Sound(int[] initSamples)  // constructor to initialize samples
+  {
+    samples = initSamples;
+  }
 
   /** Changes those values in this sound that have an amplitude greater than limit.
    *  Values greater than limit are changed to limit.
@@ -15,22 +19,18 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
-    /* to be implemented in part (a) */
     int count = 0;
-    for(int i =0;i<samples.length;i++){
-      if(samples[i]>limit){
-        samples[i]=limit;
+    for (int i = 0; i < samples.length; i++) {
+      if (samples[i] > limit) {
+        samples[i] = limit;
         count++;
-      }
-      else if (samples[i]<-limit){
+      } else if (samples[i] < -limit) {
         samples[i] = -limit;
         count++;
       }
     }
     return count;
   }
-
-
 
   /** Removes all silence from the beginning of this sound.
    *  Silence is represented by a value of 0.
@@ -39,13 +39,14 @@ public class Sound
    */
   public void trimSilenceFromBeginning()
   {
-    /* to be implemented in part (b) */
     int num = 0;
-    while(samples[num]==0)
+    while (samples[num] == 0) {
       num++;
-    int [] newSamples = new int(samples.length-num];
-    for(int i = 0;i<newSamples.length;i++)
-      newSamples[i] = samples[i+num];
+    }
+    int[] newSamples = new int[samples.length - num];
+    for (int i = 0; i < newSamples.length; i++) {
+      newSamples[i] = samples[i + num];
+    }
     samples = newSamples;
   }
 }
